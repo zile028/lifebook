da3e5ddf79656959849625c23594b3ebaeadc8e9
Danilo Vesovic <vesovicdanilo@gmail.com>
2021-08-03 18:48:44 +0200

    register errors

--- register.php
+++ register.php
@@ -11,6 +11,30 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     }else{
         $first_name = $_POST['first_name'];
     }
+    if(!isset($_POST['last_name']) || empty($_POST['last_name'])){
+        $last_name_error = "Last name is required";
+        array_push($errors,$last_name_error);
+    }else{
+        $last_name = $_POST['last_name'];
+    }
+    if(!isset($_POST['email']) || empty($_POST['email'])){
+        $email_error = "Emal name is required";
+        array_push($errors,$email_error);
+    }else{
+        $email = $_POST['email'];
+    }
+    if(!isset($_POST['password']) || empty($_POST['password'])){
+        $password_error = "Password name is required";
+        array_push($errors,$password_error);
+    }else{
+        $password = $_POST['password'];
+    }
+    if(!isset($_POST['password_repeat']) || empty($_POST['password_repeat'])){
+        $password_repeat_error = "Password name is required";
+        array_push($errors,$password_repeat_error);
+    }else{
+        $password_repeat = $_POST['password_repeat'];
+    }
 }
 
 require "views/register.view.php";
--- views/register.view.php
+++ views/register.view.php
@@ -9,14 +9,32 @@
                     <option value="mr">Mr</option>
                     <option value="ms">Ms</option>
                 </select><br>
-                <input type="text" name="first_name" placeholder="first_name" class="form-control"><br>
+                <input type="text" name="first_name" placeholder="first_name" class="form-control"
+                value="<?php if(isset($first_name)) echo $first_name ?>"
+                ><br>
                 <?php if(isset($first_name_error)): ?>
                     <p class="text-danger"><?php echo $first_name_error ?></p>
                 <?php endif; ?>
-                <input type="text" name="last_name" placeholder="last_name" class="form-control"><br>
-                <input type="text" name="email" placeholder="email" class="form-control"><br>
+                <input type="text" name="last_name" placeholder="last_name" class="form-control"
+                value="<?php if(isset($last_name)) echo $last_name ?>"
+                ><br>
+                <?php if(isset($last_name_error)): ?>
+                    <p class="text-danger"><?php echo $last_name_error ?></p>
+                <?php endif; ?>
+                <input type="text" name="email" placeholder="email" class="form-control"
+                value="<?php if(isset($email)) echo $email ?>"
+                ><br>
+                <?php if(isset($email_error)): ?>
+                    <p class="text-danger"><?php echo $email_error ?></p>
+                <?php endif; ?>
                 <input type="password" name="password" placeholder="password" class="form-control"><br>
+                <?php if(isset($password_error)): ?>
+                    <p class="text-danger"><?php echo $password_error ?></p>
+                <?php endif; ?>
                 <input type="password" name="password_repeat" placeholder="password_repeat" class="form-control"><br>
+                <?php if(isset($password_repeat_error)): ?>
+                    <p class="text-danger"><?php echo $password_repeat_error ?></p>
+                <?php endif; ?>
                 <button class="btn btn-primary form-control">Register</button>
             </form>
         </div>
